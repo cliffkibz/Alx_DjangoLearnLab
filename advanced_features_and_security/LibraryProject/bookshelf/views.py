5cb615c14b76f97b0b2535fe3b2c3ace76c043b8
Wycliffe cheruiyot <53683890+cliffkibz@users.noreply.github.com>
2025-09-12 20:18:10 +0300

    changes in the views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from django import forms
+from .forms import ExampleForm
 
 # Book Form
 class BookForm(forms.ModelForm):
