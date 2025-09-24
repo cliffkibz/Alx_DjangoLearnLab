def385497567fd319e6a3c54807fe12e8f4d3227
Wycliffe cheruiyot <53683890+cliffkibz@users.noreply.github.com>
2025-09-24 17:44:57 +0300

    major changes

--- settings.py
+++ settings.py
@@ -1,2 +1,12 @@
-# This file is a symlink to the actual settings.py for compatibility with checkers
+<<<<<<< HEAD
 from settings import *
+=======
+<<<<<<< HEAD
+from api_project.settings import *
+=======
+from settings import *
+>>>>>>> 3cec29e (major changes)
+>>>>>>> 5a368e7 (major changes)
+
+# This file is a symlink to the actual settings.py for compatibility with checkers
+from api_project.settings import *
