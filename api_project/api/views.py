a868d28913cc1d4053ed3216b630f3084d75d801
Wycliffe cheruiyot <53683890+cliffkibz@users.noreply.github.com>
2025-09-25 14:35:25 +0300

    h

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,19 @@
-from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+from rest_framework import generics, viewsets
+from rest_framework import permissions
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# ViewSet for CRUD operations
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
